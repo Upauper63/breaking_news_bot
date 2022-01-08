509e2ec17627d15cdd23c5d70973f8b5a39f8672
Upauper63 <upauper63@gmail.com>
2022-01-08 21:36:35 +0900

    timezone fix

--- breaking_news_bot.py
+++ breaking_news_bot.py
@@ -8,7 +8,7 @@ import feedparser
 JVNから新着情報を取得
 
 """
-with open('stopItem.yaml') as f:
+with open('breaking_news_bot.yaml') as f:
     yml = yaml.safe_load(f)
     stop_item = yml['stopItem']
     start_item = str(stop_item + 1)
@@ -33,7 +33,7 @@ for entry in cont['feed']['entry']:
     stop_item += 1
     time.sleep(1)
 
-with open('stopItem.yaml', 'w') as f:
+with open('breaking_news_bot.yaml', 'w') as f:
     yml = {'stopItem': stop_item, 'webhookUrl': webhook_url}
     yaml.safe_dump(yml, f)
 
@@ -48,20 +48,19 @@ no_news = True
 entries_container = []
 for entry in cont['entries']:
     entry_date = datetime.datetime.fromisoformat(entry['updated'])
-    if entry_date >= datetime.datetime.now() - datetime.timedelta(days=1):
+    if entry_date >= datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))) - datetime.timedelta(days=1):
         no_news = False
         message_title = entry['title'] + '【IPA】'
         message_text = '更新日：' + entry_date.strftime('%Y-%m-%d %H:%M:%S') + '</br>'
         message_text += "[関連情報](" + entry['link'] + ")"
         entries_container.append({'entry_date': entry_date, 'message_title': message_title, 'message_text': message_text})
 
-entries_container = sorted(entries_container, key=lambda x: x['entry_date'])
-
 if no_news:
     message = pymsteams.connectorcard(webhook_url)
     message.text('新しい情報は入ってきていません。【IPA】')
     message.send()
 
+entries_container = sorted(entries_container, key=lambda x: x['entry_date'])
 for entry in entries_container:
     message = pymsteams.connectorcard(webhook_url)
     message.title(entry['message_title'])
